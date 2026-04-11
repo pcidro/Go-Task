38ee9c7e41e1d441d86595a2b8477194f5ac3151
Paulo Cidro <paulocidro@gmail.com>
2026-04-11 20:00:41 -0300

    finalizando o layout com tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+import { TaskFormModalComponent } from './components/task-form-modal/task-form-modal.component';
+import { TasksCommentsModalComponent } from './components/tasks-comments-modal/tasks-comments-modal.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [
+    HeaderComponent,
+    MainContentComponent,
+    TaskFormModalComponent,
+    TasksCommentsModalComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
